eae6a420630d314bb292e1396417490ba9a1836a
Shota-Yukawa <71016455+Shota-Yukawa@users.noreply.github.com>
2023-08-07 22:29:44 +0900

    ブランチ名変更。baseInfoからresidence

--- residence/src/main/java/com/ah/baseInfo/ResidenceApplication.java
+++ residence/src/main/java/com/ah/baseInfo/ResidenceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseInfoApplication {
+public class ResidenceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BaseInfoApplication.class, args);
+		SpringApplication.run(ResidenceApplication.class, args);
 	}
 
 }
